681c587b624070dec2250bebec843576c9d362d9
Fnoefen <70907439+Fnoefen@users.noreply.github.com>
2022-04-12 21:01:38 +0200

    Main menu created

---
+++ RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Connect.kt
@@ -0,0 +1,11 @@
+package com.example.rc_car_controller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Connect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect)
+    }
+}
