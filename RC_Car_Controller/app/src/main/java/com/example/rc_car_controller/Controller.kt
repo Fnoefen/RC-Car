09978e707f752a3499f0a3031a5d36f7a3a920f9
Fnoefen <70907439+Fnoefen@users.noreply.github.com>
2022-05-18 16:47:32 +0200

    fixed angel calculation

--- RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Controller.kt
+++ RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Controller.kt
@@ -125,11 +125,12 @@ class Controller : AppCompatActivity(), JoystickView.JoystickListener {
                 speed = findViewById<TextView>(R.id.speed)
                 val speedVal: Int = (sqrt(yPercent.pow(2) + xPercent.pow(2)) * 100).toInt()
                 var angelVal: Int = 0
-                if(xPercent > 0)
+                if(xPercent > 0 && xPercent != 0F)
                     angelVal = 90 - ((atan(abs(yPercent)/xPercent) * 180) / PI).roundToInt()
-                else
+                else if (xPercent != 0F)
                     angelVal = ((atan(abs(yPercent)/abs(xPercent)) * 180) / PI).roundToInt() - 90
-
+                else
+                    angelVal = 0
                 angel.text = getString(R.string.angel) + angelVal
                 speed.text = getString(R.string.speed) + speedVal
             }
