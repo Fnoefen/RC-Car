1fc9dd554b8ad407766782a9e97ef5d668aed5ad
Fnoefen <70907439+Fnoefen@users.noreply.github.com>
2022-05-18 14:37:26 +0200

    design + discornect + bluetooth stuff

--- RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Controller.kt
+++ RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Controller.kt
@@ -17,20 +17,21 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.IOException
 import java.util.*
 
 class Controller : AppCompatActivity() {
 
-    lateinit var angelCtrl : SeekBar
-    lateinit var angel : TextView
+    lateinit var angelCtrl: SeekBar
+    lateinit var angel: TextView
 
-    lateinit var speedCtrl : SeekBar
-    lateinit var speed : TextView
+    lateinit var speedCtrl: SeekBar
+    lateinit var speed: TextView
 
-    lateinit var testBTN : Button
+    lateinit var testBTN: Button
 
-
+    lateinit var discornect : FloatingActionButton
 
 
     companion object {
@@ -39,19 +40,20 @@ class Controller : AppCompatActivity() {
         lateinit var m_progress: ProgressDialog
         lateinit var m_bluetoothAdapter: BluetoothAdapter
         var m_isConnected: Boolean = false
-        var m_address: String ="some address"
+        var m_address: String = "some address"
 
         //  Values to send over bluetooth ---
-        var mSpeed : String = " "
-        var mAngel : String = " "
-        //  -----
-        var testvar :Boolean = false;
+        var mSpeed: String = " "
+        var mAngel: String = " "
 
-        fun sendCommand(input: String){
-            if(m_bluetoothSocket != null){
-                try{
+        //  -----
+        var testvar: Boolean = false;
+
+        fun sendCommand(input: String) {
+            if (m_bluetoothSocket != null) {
+                try {
                     m_bluetoothSocket!!.outputStream.write(input.toByteArray())
-                } catch(e: IOException) {
+                } catch (e: IOException) {
                     e.printStackTrace()
                 }
             }
@@ -64,43 +66,56 @@ class Controller : AppCompatActivity() {
         setContentView(R.layout.activity_controller)
 
         m_address = Connect.EXTRA_ADRESS
-        if(m_address == "Device_address" ){
+        if (m_address == "Device_address") {
             Toast.makeText(applicationContext, "No device connected", Toast.LENGTH_SHORT).show()
-        }else{
+        } else {
             ConnectToDevice(this).execute()
         }
-
-        angelCtrl = findViewById<SeekBar>(R.id.AngelCtrl)
-        angel = findViewById<TextView>(R.id.Angel)
-
-        angelCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                angel.text = p1.toString()
-                mAngel = p1.toString()
-            }
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-                angelCtrl.progress = 0
-            }
-
-        })
-
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            speedCtrl = findViewById<SeekBar>(R.id.SpeedCtrl)
-            speed = findViewById<TextView>(R.id.Speed)
+            angelCtrl = findViewById<SeekBar>(R.id.AngelCtrl)
+            angel = findViewById<TextView>(R.id.angel2)
+            angelCtrl.thumb.mutate().alpha = 0;
 
-            speedCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            angelCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                    speed.text = p1.toString()
+                    angel.text = getString(R.string.speed) + " $p1"
+                    mAngel = p1.toString()
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                    angelCtrl.progress = 0
+                }
+
+            })
+
+
+            speedCtrl = findViewById<SeekBar>(R.id.SpeedCtrl)
+            speed = findViewById<TextView>(R.id.speed2)
+            speedCtrl.thumb.mutate().alpha = 0;
+
+            speedCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    speed.text = getString(R.string.speed) + " $p1"
                     mSpeed = p1.toString()
                 }
+
                 override fun onStartTrackingTouch(p0: SeekBar?) {}
                 override fun onStopTrackingTouch(p0: SeekBar?) {
                     speedCtrl.progress = 0
                 }
             })
+            discornect = findViewById<FloatingActionButton>(R.id.disconnect)
+            discornect.setOnClickListener{
+                if(m_isConnected){
+                    disconnect()
+                }else {
+                    Toast.makeText(applicationContext, "You are not connected to any device", Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             testBTN = findViewById<Button>(R.id.TestBTN)
             testBTN.setOnClickListener { disconnect() }
 
@@ -111,44 +126,41 @@ class Controller : AppCompatActivity() {
     }
 
     //  Thread ------------------------------------------------------------
-    class thread() :Thread() {
-        var prevSpeed : String = " "
-        var prevAngel : String = " "
+    class thread() : Thread() {
+        var prevSpeed: String = " "
+        var prevAngel: String = " "
 
         override fun run() {
             try {
-                while(m_isConnected){
-                    if(mSpeed != prevSpeed){
+                while (m_isConnected) {
+                    if (mSpeed != prevSpeed) {
                         sendCommand(mSpeed)
                         prevSpeed = mSpeed
                     }
-                    if(mAngel != prevAngel){
+                    if (mAngel != prevAngel) {
                         sendCommand(mAngel)
                         prevAngel = mAngel
                     }
                 }
-                while(!m_isConnected){
-                    if(mSpeed != prevSpeed){
+                while (!m_isConnected) {
+                    if (mSpeed != prevSpeed) {
                         println(mSpeed)
                         sendCommand(mSpeed)
                         prevSpeed = mSpeed
                     }
-                    if(mAngel != prevAngel){
+                    if (mAngel != prevAngel) {
                         println(mAngel)
                         sendCommand(mAngel)
                         prevAngel = mAngel
                     }
                 }
-            }catch (ex:Exception){
+            } catch (ex: Exception) {
                 print(ex.message)
             }
         }
-
-
-
     }
 
-    private fun disconnect(){
+    private fun disconnect() {
         if (m_bluetoothSocket != null) {
             try {
                 m_bluetoothSocket!!.close()
@@ -161,10 +173,7 @@ class Controller : AppCompatActivity() {
         finish()
     }
 
-
-
-
-    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>(){
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
         private var connectSuccess: Boolean = true
         private val context: Context
 
@@ -183,14 +192,26 @@ class Controller : AppCompatActivity() {
                 if (m_bluetoothSocket == null || !m_isConnected) {
                     m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
                     val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
-                    if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
-                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_UUID)
+                    if (ActivityCompat.checkSelfPermission(
+                            this.context,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    )
+                        m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_UUID)
 
-                    if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED)
-                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    if (ActivityCompat.checkSelfPermission(
+                            this.context,
+                            Manifest.permission.BLUETOOTH_SCAN
+                        ) != PackageManager.PERMISSION_GRANTED
+                    )
+                        BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
 
-                    if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
-                    m_bluetoothSocket!!.connect()
+                    if (ActivityCompat.checkSelfPermission(
+                            this.context,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    )
+                        m_bluetoothSocket!!.connect()
                 }
             } catch (e: IOException) {
                 connectSuccess = false
