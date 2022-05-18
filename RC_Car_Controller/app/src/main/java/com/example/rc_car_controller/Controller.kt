e5f3233f5774776a85870866ea3206e08bee6d2c
Christoffer Lyholm <72018912+LePrivateRyan@users.noreply.github.com>
2022-05-18 14:55:03 +0200

    Joystick
    It works now and you can change the color gradiant and get the procent x and y postion of the joystick

--- RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Controller.kt
+++ RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Controller.kt
@@ -3,13 +3,12 @@ package com.example.rc_car_controller
 import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
-class  Controller : AppCompatActivity(){
+class Controller : AppCompatActivity(), JoystickView.JoystickListener {
 
     lateinit var angelCtrl: SeekBar
     lateinit var angel: TextView
@@ -17,78 +16,56 @@ class  Controller : AppCompatActivity(){
     lateinit var speedCtrl: SeekBar
     lateinit var speed: TextView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Joystick
-        var joystick = Joystick(this)
-        setContentView(joystick)
+        setContentView(R.layout.activity_controller)
 
+        // Landscape controller
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            angelCtrl = findViewById<SeekBar>(R.id.AngelCtrl)
+            angel = findViewById<TextView>(R.id.Angel)
 
+            angelCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    angel.text = p1.toString()
+                }
 
-//        // Landscape controller
-//        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-//            angelCtrl = findViewById<SeekBar>(R.id.AngelCtrl)
-//            angel = findViewById<TextView>(R.id.Angel)
-//
-//            angelCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-//                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-//                    angel.text = p1.toString()
-//                }
-//
-//                override fun onStartTrackingTouch(p0: SeekBar?) {}
-//                override fun onStopTrackingTouch(p0: SeekBar?) {
-//                    angelCtrl.progress = 0
-//                }
-//            })
-//            speedCtrl = findViewById<SeekBar>(R.id.SpeedCtrl)
-//            speed = findViewById<TextView>(R.id.Speed)
-//
-//            speedCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-//                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-//                    speed.text = p1.toString()
-//                }
-//
-//                override fun onStartTrackingTouch(p0: SeekBar?) {}
-//                override fun onStopTrackingTouch(p0: SeekBar?) {
-//                    speedCtrl.progress = 0
-//                }
-//            })
-//        }
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                    angelCtrl.progress = 0
+                }
+            })
+            speedCtrl = findViewById<SeekBar>(R.id.SpeedCtrl)
+            speed = findViewById<TextView>(R.id.Speed)
 
+            speedCtrl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    speed.text = p1.toString()
+                }
 
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                    speedCtrl.progress = 0
+                }
+            })
+
+        }
     }
 
-    fun onJoystickMoved(xPercent: Float, yPercent: Float, source: Int){
-        Log.d("Main Method", "X percent: " + xPercent + "Y percent: " + yPercent)
+    override fun onJoystickMoved(xPercent: Float, yPercent: Float, id: Int) {
+        when(id){
+            R.id.joystickViewUp ->{
+                Log.d("Right joystick", "X percent: " + xPercent + "Y percent: " + yPercent)
+                angel = findViewById<TextView>(R.id.angle)
+                speed = findViewById<TextView>(R.id.speed)
+                angel.text = yPercent.toString()
+                speed.text = xPercent.toString()
+            }
+            R.id.joystickView3 ->{
+                Log.d("Left joystick", "X percent: " + xPercent + "Y percent: " + yPercent)
+            }
+        }
+
     }
-
-//    override fun onTouchEvent(event: MotionEvent?): Boolean {
-//
-//        if (event != null) when (event.action) {
-//            MotionEvent.ACTION_DOWN -> {
-//                if (joystick.isPressed(event.x.toDouble(), event.y.toDouble())) {
-//                    joystick.setIsPressed(true)
-//                }
-//                return true
-//            }
-//            MotionEvent.ACTION_MOVE -> {
-//                if (joystick.getIsPressed()) {
-//                    joystick.setActuator(event.x.toDouble(), event.y.toDouble())
-//                }
-//                return true
-//            }
-//            MotionEvent.ACTION_UP -> {
-//                joystick.setIsPressed(false)
-//                joystick.resetActuator()
-//                return true
-//            }
-//        }
-//        return super.onTouchEvent(event)
-//
-//    }
-
-
-
 
 }
