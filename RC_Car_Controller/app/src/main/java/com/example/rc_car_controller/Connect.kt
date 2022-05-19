574345d5b92c4711a9e1e4ab06eee0a6ab6a046a
Fnoefen <70907439+Fnoefen@users.noreply.github.com>
2022-05-19 10:35:04 +0200

    now maybe?

--- RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Connect.kt
+++ RC_Car_Controller/app/src/main/java/com/example/rc_car_controller/Connect.kt
@@ -19,7 +19,7 @@ class Connect : AppCompatActivity() {
     private lateinit var m_pairedDevices: Set<BluetoothDevice>
     private var REQUEST_ENABLE_BLUETOOTH = 1
 
-    private lateinit var testbutton : Button
+    private lateinit var testbutton: Button
 
     companion object {
         var EXTRA_ADRESS: String = "Device_address"
@@ -33,18 +33,22 @@ class Connect : AppCompatActivity() {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
         m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (m_bluetoothAdapter == null){
-            Toast.makeText(applicationContext, "This device dose not support Bluetooth", Toast.LENGTH_SHORT).show()
+        if (m_bluetoothAdapter == null) {
+            Toast.makeText(
+                applicationContext,
+                "This device dose not support Bluetooth",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
-        if (!m_bluetoothAdapter!!.isEnabled){
+        if (!m_bluetoothAdapter!!.isEnabled) {
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             if (ActivityCompat.checkSelfPermission(
                     this,
                     Manifest.permission.BLUETOOTH_CONNECT
                 ) != PackageManager.PERMISSION_GRANTED
             )
-            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+                startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
         }
 
         val refbutton: Button = findViewById(R.id.SelectDeviceRefresh)
@@ -61,53 +65,72 @@ class Connect : AppCompatActivity() {
                 Manifest.permission.BLUETOOTH_CONNECT
             ) != PackageManager.PERMISSION_GRANTED
         )
-        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+            m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
         val list: ArrayList<BluetoothDevice> = ArrayList()
 
-        if(m_pairedDevices.isNotEmpty()){
-            for(device: BluetoothDevice in m_pairedDevices){
+        if (m_pairedDevices.isNotEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
                 list.add(device)
-                Log.i("device",""+device)
+                Log.i("device", "" + device)
             }
-        }else {
-            Toast.makeText(applicationContext, "No paired Bluetooth devices found", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "No paired Bluetooth devices found",
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
 //        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,list)
 
         val listNames: ArrayList<String> = ArrayList()
 
-                        //for device name
-                        for(device: BluetoothDevice in list){
-                            listNames.add(device.name)
-                        }
-                        val adapter = ArrayAdapter(this,R.layout.listview_item,listNames)
+        //for device name
+        for (device: BluetoothDevice in list) {
+            listNames.add(device.name)
+        }
+        val adapter = ArrayAdapter(this, R.layout.listview_item, listNames)
 
         val selectDeviceList: ListView = findViewById(R.id.SelectDeviceList)
         selectDeviceList.adapter = adapter
-        selectDeviceList.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val device: BluetoothDevice = list[position]
-            val address: String = device.address
-            EXTRA_ADRESS = address
 
-            val intent = Intent(this, MainActivity::class.java)
-            //intent.putExtra(EXTRA_ADRESS, address)
-            startActivity(intent)
-        }
+        selectDeviceList.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val device: BluetoothDevice = list[position]
+                val address: String = device.address
+                EXTRA_ADRESS = address
+
+                val intent = Intent(this, MainActivity::class.java)
+                //intent.putExtra(EXTRA_ADRESS, address)
+                startActivity(intent)
+            }
 
     }
-//  Toast logic - prompts
+
+    //  Toast logic - prompts
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_ENABLE_BLUETOOTH){
-            if (resultCode == Activity.RESULT_OK){
-                if (m_bluetoothAdapter!!.isEnabled){
-                    Toast.makeText(applicationContext, "Bluetooth has been enabled", Toast.LENGTH_SHORT).show()
-                }else {
-                    Toast.makeText(applicationContext, "Bluetooth has been disabled", Toast.LENGTH_SHORT).show()
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Bluetooth has been enabled",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Bluetooth has been disabled",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-            }else if (resultCode == Activity.RESULT_CANCELED){
-                Toast.makeText(applicationContext, "Bluetooth enabling has been canceled", Toast.LENGTH_SHORT).show()
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(
+                    applicationContext,
+                    "Bluetooth enabling has been canceled",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
